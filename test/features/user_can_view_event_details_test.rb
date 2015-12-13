3e82966c4becc50aae44d0cc8243df9fecf67723
James Crockett <jecrockett@gmail.com>
2015-12-13 15:54:32 -0700

    adding more stil

--- test/features/user_can_view_event_details_test.rb
+++ test/features/user_can_view_event_details_test.rb
@@ -19,10 +19,21 @@ class UserCanViewEventDetailsTest < FeatureTest
     ph = PayloadHandler.new(payload)
 
     visit '/sources/iguanas/events/socialLogin'
-    save_and_open_page
 
     within '#error-message' do
       assert page.has_content?("The identifier does not exist.")
+    end
+  end
+
+  def test_user_cannot_view_event_details_for_event_that_hasnt_been_recorded
+    Client.create({"name" => "jumpstartlab", "root_url" => "http://jumpstartlab.com"})
+    ph = PayloadHandler.new(payload)
+
+    visit '/sources/jumpstartlab/events/makingtaylorcryaftermethodicallydisassemblinghiminsupersmashbrothers'
+    save_and_open_page
+
+    within '#error-message' do
+      assert page.has_content?("That event isn't defined.")
     end
   end
 
